e1512ac2fe5508fe09b79187406cc0dd4702cbde
Tam Duong <tam.duong@kindhelm.com>
2020-07-15 09:53:00 +0700

    wrap up

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/goinggo/tracelog"
@@ -23,7 +24,7 @@ type IonPumpDataPoint struct {
 type FlipperDewPoint struct {
 	FlipperID   string  `json:"FID"`
 	Channel     int     `json:"CH"`
-	Temperature float32 `json:"TEMP"`
+	Temperature float64 `json:"TEMP"`
 	Timestamp   int64   `json:"T"`
 	DepotID     string  `json:"DID"`
 }
@@ -51,9 +52,9 @@ func main() {
 	tracelog.Start(tracelog.LevelInfo)
 
 	// tracelog.StartFile(tracelog.LevelWarn, "collectorLog", 5)
-	inFluxClient = influxdb2.NewClient("http://dynim.ddns.net:8800", "QVyFUZnEGrI3jOwTPbrSpOIGbBacd_OTk8NdD6b9aEPVJW1Ttxe5QiULGqF5TNzNCJM7NjLUUMOSDK971Mzzaw==")
+	inFluxClient = influxdb2.NewClient("http://localhost:9999", "v8JhGRu6SxrCHNVAluyrhAOv-21YZn3NwouvtNwe-a2VVLokXaK9BdEu2sgbUNdXFYqi7xQLKwi2ICTDGzAF5w==")
 
-	writeApi = inFluxClient.WriteApiBlocking("Kindhelm", "tam")
+	writeApi = inFluxClient.WriteApiBlocking("KLA", "udc")
 
 	defer func() {
 
@@ -63,11 +64,37 @@ func main() {
 	}()
 
 	for {
-		newDataPoint := IonPumpDataPoint{DepotID: "UDC", PumpID: "00-01", Timestamp: time.Now().Unix(), LPN: "SU-000006E9DAE4", Current: randFloats(10e-9, 0.4, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+		newDataPoint := IonPumpDataPoint{DepotID: "UDC", PumpID: "00-01", Timestamp: time.Now().Unix(), LPN: "SU-000006E9DAE4", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
 
 		sendIonPumpDataPoint(&newDataPoint, &writeApi)
-		newDataPoint = IonPumpDataPoint{DepotID: "UDC", PumpID: "00-01", Timestamp: time.Now().Unix(), LPN: "SU-000007D6D6B9", Current: randFloats(10e-9, 0.4, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+		newDataPoint = IonPumpDataPoint{DepotID: "UDC", PumpID: "00-02", Timestamp: time.Now().Unix(), LPN: "SU-000007D6D6B9", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+
 		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+		newDataPoint = IonPumpDataPoint{DepotID: "UDC", PumpID: "01-01", Timestamp: time.Now().Unix(), LPN: "SU-000007D6D6T8", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+
+		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+		newDataPoint = IonPumpDataPoint{DepotID: "UDC", PumpID: "01-02", Timestamp: time.Now().Unix(), LPN: "SU-000007D6D6T7", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+
+		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+		newDataPoint = IonPumpDataPoint{DepotID: "UDC", PumpID: "02-01", Timestamp: time.Now().Unix(), LPN: "SU-000007D6D68T", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+
+		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+		newDataPoint = IonPumpDataPoint{DepotID: "UDC", PumpID: "02-02", Timestamp: time.Now().Unix(), LPN: "SU-000007D6D6AA", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+
+		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+		newDataPoint = IonPumpDataPoint{DepotID: "TKO", PumpID: "00-01", Timestamp: time.Now().Unix(), LPN: "SU-000007D66838", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+		newDataPoint = IonPumpDataPoint{DepotID: "TKO", PumpID: "01-03", Timestamp: time.Now().Unix(), LPN: "SU-000007D62234", Current: randFloats(10e-9, 10e-6, 1)[0], Pressure: randFloats(10e-12, 10e-7, 1)[0], Voltage: float64(rand.Intn(7000-0) + 0)}
+		sendIonPumpDataPoint(&newDataPoint, &writeApi)
+
+		flipperPoint := FlipperDewPoint{DepotID: "UDC", Channel: 1, Temperature: randFloats(-100, 0, 1)[0], Timestamp: time.Now().Unix(), FlipperID: "0001"}
+		sendFlipperDatapoint(&flipperPoint, &writeApi)
+
+		flipperPoint = FlipperDewPoint{DepotID: "UDC", Channel: 2, Temperature: randFloats(-100, 0, 1)[0], Timestamp: time.Now().Unix(), FlipperID: "0002"}
+		sendFlipperDatapoint(&flipperPoint, &writeApi)
+
+		flipperPoint = FlipperDewPoint{DepotID: "TKO", Channel: 1, Temperature: randFloats(-100, 0, 1)[0], Timestamp: time.Now().Unix(), FlipperID: "0003"}
+		sendFlipperDatapoint(&flipperPoint, &writeApi)
 		// time.Sleep(time.Minute)
 		time.Sleep(10 * time.Second)
 	}
@@ -83,6 +110,22 @@ func sendIonPumpDataPoint(dataPoint *IonPumpDataPoint, writeAPI *api.WriteApiBlo
 	p.AddField("Current", dataPoint.Current)
 	p.AddField("Pressure", dataPoint.Pressure)
 	p.SetTime(time.Unix(dataPoint.Timestamp, 0))
+
+	err := (*writeAPI).WritePoint(context.Background(), p)
+
+	if err != nil {
+		tracelog.Error(err, "write to DB fail", "IonPump")
+	}
+}
+
+func sendFlipperDatapoint(flipperPoint *FlipperDewPoint, writeAPI *api.WriteApiBlocking) {
+
+	p := influxdb2.NewPointWithMeasurement("Flipper")
+	p.AddTag("DepotID", flipperPoint.DepotID)
+	p.AddTag("Channel", strconv.Itoa(flipperPoint.Channel))
+	p.AddTag("FlipperID", flipperPoint.FlipperID)
+	p.AddField("Temperature", flipperPoint.Temperature)
+	p.SetTime(time.Unix(flipperPoint.Timestamp, 0))
 
 	err := (*writeAPI).WritePoint(context.Background(), p)
 
